fca8c73a1a05b5b08f474d3f06a69a33453a10ac
Adrian <adgo0001@stud.kea.dk>
2025-03-03 11:55:57 +0100

    Initial commit

---
+++ src/main/java/com/example/kinofullstack/KinoFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kinofullstack/KinoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
